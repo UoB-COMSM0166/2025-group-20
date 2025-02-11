a4b2e9ad09b11c41e7433dc8533de1ef2e9f4c94
Scarlett Hurford <cy21903@bristol.ac.uk>
2025-02-11 16:21:36 +0000

    js

--- docs/Game code/sketch.js
+++ docs/Game code/sketch.js
@@ -1,18 +1,21 @@
-let state = 0;
+
+let gameState = "intro";
 let playButton;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  playButton = createButton("Start Game")
 }
 
 function draw() {
   background('rgb(100%, 0%, 10%)');
+
   textAlign(CENTER);
   textSize(50);
   text('START GAME', width / 2, height / 2 + 10);
-}
+}  
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
