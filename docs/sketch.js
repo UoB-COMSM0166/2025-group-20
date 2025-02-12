a29ed569703f262fd54c3fd00e089b2c24c0fee8
May Daoud <maydaoud@IsabellasiPhone.localdomain>
2025-02-12 23:17:59 +0000

    add global variables

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,14 +1,24 @@
-var mode;
+// global variables
+var mode = 0;
 let gameFont, appleImg;
+// array of the fruits and vegetables 
+var fruitList = ['apple', 'banana', 'blueberry', 'carrot', 'cherry', 'grape', 'watermelon', 'bomb'];
+var fruitImgs = [];
 
 function preload() {
+  // loads material used in start screen
   gameFont = loadFont('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/gameFont.otf');
   appleImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/fruits/apple.png');
+  
+  // loads fruit images to the fruitImgs array
+  for (var i = 0; i < fruitList.length; i++) {
+    fruitImgs[i] = loadImage('Images/' + fruitList[i] + '.png');
+  }
 }
 
 function setup() {
-  mode = 0;
   createCanvas(windowWidth, windowHeight);
+  frameRate(60); // most computers default to 60fps
 }
 
 function draw() {
