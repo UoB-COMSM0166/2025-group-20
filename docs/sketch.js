7cc87a0da4e1228905f2f4ad981d95ee2cc8622a
May Daoud <zy21368@bristol.ac.uk>
2025-02-17 15:04:14 +0000

    replace heart in code

--- docs/sketch.js
+++ docs/sketch.js
@@ -16,8 +16,8 @@ function preload() {
   gameFont = loadFont('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/gameFont.otf');
   appleImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/apple.png');
   bg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/923cd18c3e0c776d146c9cb4e9bf10b24d488e40/docs/Background%20Images/Game%20Screen%20Background.png');
-  lifeImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/54b989cf2c28d627c787aa7f95a2c2dc414c2589/docs/Images/life.png');
-  lifelostImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/54b989cf2c28d627c787aa7f95a2c2dc414c2589/docs/Images/lifelost.png');
+  lifeImg = loadImage('https://raw.github.com/UoB-COMSM0166/2025-group-20/blob/5ce3bfd96e5c548782dd8be6afe839346ab5babc/docs/Images/heart.png');
+  lifelostImg = loadImage('https://raw.github.com/UoB-COMSM0166/2025-group-20/blob/5ce3bfd96e5c548782dd8be6afe839346ab5babc/docs/Images/heart-empty.png');
   
   // loads fruit images to the fruitImgs array
   for (var i = 0; i < fruitList.length; i++) {
