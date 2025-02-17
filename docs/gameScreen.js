ae9f9c89093274785cc2321b74e67b88f755a973
bever1tbev <ih21562@bristol.ac.uk>
2025-02-17 19:30:49 +0000

    Update gameScreen.js

--- docs/gameScreen.js
+++ docs/gameScreen.js
@@ -12,16 +12,29 @@ function gameScreen() {
     for (var i = fruit.length - 1; i >= 0; i--) {
         fruit[i].show();
         fruit[i].move();
-        
-        // if (fruit[i].slicePat.isSliced() == 'correct'){
-        //      //gain points
-        //  }
-        //  else if (fruit[i].slicePat.isSliced() == 'wrong'){
-        //      //lose life
-        //  }
-        //  else if (fruit[i].slicePat.isSliced() == 'bomb'){
-        //      //game over
-        //  }
+        if (fruit[i].slicePat.isSliced() == 'correct' || fruit[i].slicePat.isSliced() == 'wrong'){
+            if (fruit[i] != currentRecipe.ingredients[0]){
+                // lose life
+            }
+            else {
+                if (fruit[i].slicePat.isSliced() == 'correct'){
+                    currentRecipe.ingredients.shift();
+                    pointsystem.correctCut();
+                    fruitImgs[i] = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/' + fruitList[i] + '-slice.png');
+                    fruit[i].slicePat = null;
+                }
+                else if (fruit[i].slicePat.isSliced() == 'wrong'){
+                    //wrong slice effect
+                    wrongSliceFrame.redBorder();
+                    fruitImgs[i] = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/' + fruitList[i] + '-slice.png');
+                    fruit[i].slicePat = null;
+                }
+            }
+        }
+        else if (fruit[i].slicePat.isSliced() == 'bomb'){
+            //game over
+            //switch to exploded screen
+        }
     }
 
     if (!smoothieDisplay && fruitImgs.every(img => img instanceof p5.Image)) {
