0177072be039adcb806eeef6bfb9f4622b5459ef
bever1tbev <ih21562@bristol.ac.uk>
2025-02-18 16:58:41 +0000

    Updated Game Screen and sketch

--- docs/sketch.js
+++ docs/sketch.js
@@ -2,41 +2,33 @@
 var mode = 0;
 let gameFont, appleImg;
 var gravity = 0.1;
-var displayBorder = false;
 // array of the fruits and vegetables 
-var fruitList = ['apple', 'banana', 'blueberry', 'pear', 'cherry', 'grape', 'watermelon', 'lemon','bomb']; //one more fruit needed
+var fruitList = ['apple', 'banana', 'blueberry', 'carrot', 'cherry', 'grape', 'watermelon', 'bomb']; //one more fruit needed
 var sliceList = ['up', 'down','click', 'left', 'right', 'lrdown/rlup', 'rldown/lrup', 'bomb']; //line up exactly with corresponding fruit above
 var fruitImgs = [];
 var fruit = [];
 let bg;
-let smoothieDisplay; 
 var lifeIcons;
-let fruitImages = []
-
-let PointsDisplay;
-let HighestPointDisplay;
+let currentRecipe;
 
 function preload() {
   // loads material used in start screen
   gameFont = loadFont('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/gameFont.otf');
   appleImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/apple.png');
   bg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/923cd18c3e0c776d146c9cb4e9bf10b24d488e40/docs/Background%20Images/Game%20Screen%20Background.png');
-  lifeImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/heart.png');
-  lifelostImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/heart-empty.png');
+  lifeImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/54b989cf2c28d627c787aa7f95a2c2dc414c2589/docs/Images/life.png');
+  lifelostImg = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/54b989cf2c28d627c787aa7f95a2c2dc414c2589/docs/Images/lifelost.png');
   
   // loads fruit images to the fruitImgs array
-  for (let i = 0; i < fruitList.length; i++) {
-    let fruitName = fruitList[i];
-    let img = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/' + fruitName + '.png');
-    fruitImgs.push(img);
-    //fruitImages[fruitName] = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/' + fruitList[i] + '.png');
+  for (var i = 0; i < fruitList.length; i++) {
+    fruitImgs[i] = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/' + fruitList[i] + '.png');
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(60); // most computers default to 60fps
-  lifeIcons = new LifeIcons(3);
+  lifeIcons = new LifeIcons();
 }
 
 function draw() {
@@ -49,7 +41,6 @@ function draw() {
   }
   if (mode == 2){
     gameScreen();
-    console.log("Game Screen loaded successfully");
   }
 }
 
