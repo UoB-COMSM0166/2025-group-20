989a25d652caf49f1f188646139434fb21cbf9f5
bever1tbev <ih21562@bristol.ac.uk>
2025-02-19 00:35:23 +0000

    Update wrongSliceFrame.js

--- docs/wrongSliceFrame.js
+++ docs/wrongSliceFrame.js
@@ -1,12 +1,11 @@
 function redBorder() {
-  if(displayBorder === true){
+  if(displayBorder == true){
+    push();
     noFill();
     stroke("red");
     strokeWeight(20);
     rectMode(CORNER);
     rect(0, 0, width, height, 20);
-    setTimeout(() => {
-      displayBorder = false;
-    }, 3000);
+    pop();
   }
 }
