bedd81d35e138d6d61eed6e76ab9ac9146c863af
May Daoud <zy21368@bristol.ac.uk>
2025-04-01 13:55:14 +0100

    Really quick pat images for testing I promise

--- docs/recipeBookButton.js
+++ docs/recipeBookButton.js
@@ -40,15 +40,18 @@ function recipeScreen() {
     fill(252, 243, 207, 180); 
     rectMode(CENTER);
     rect(windowWidth / 2, windowHeight / 2, windowWidth / 2, windowHeight / 2, 55); 
-
-    image(fruitImgs[0], windowWidth / 2 - 120, windowHeight / 2, 100, 100); // apple
-    image(fruitImgs[1], windowWidth / 2, windowHeight / 2, 100, 100); // banana
-    image(fruitImgs[2], windowWidth / 2, windowHeight / 2 - 120, 100, 100); // blueberry 
-    image(fruitImgs[3], windowWidth / 2 - 120, windowHeight / 2 - 120, 100, 100); // lemon
-    image(fruitImgs[4], windowWidth / 2 + 120, windowHeight / 2, 100, 100); // cherry
-    image(fruitImgs[5], windowWidth / 2 - 240, windowHeight / 2 - 120, 100, 100); // grapes
-    image(fruitImgs[6], windowWidth / 2 - 240, windowHeight / 2, 100, 100); // watermelon 
-    image(fruitImgs[7], windowWidth / 2 + 120, windowHeight / 2 - 120, 100, 100); // bomb just to make it even
+    var patImgs = [];
+    for (var i = 0; i < fruitList.length; i++) {
+        fruitImgs[i] = loadImage('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/Images/' + fruitList[i] + 'Pat.png');
+    }
+    image(patImgs[0], windowWidth / 2 - 120, windowHeight / 2, 100, 100); // apple
+    image(patImgs[1], windowWidth / 2, windowHeight / 2, 100, 100); // banana
+    image(patImgs[2], windowWidth / 2, windowHeight / 2 - 120, 100, 100); // blueberry 
+    image(patImgs[3], windowWidth / 2 - 120, windowHeight / 2 - 120, 100, 100); // lemon
+    image(patImgs[4], windowWidth / 2 + 120, windowHeight / 2, 100, 100); // cherry
+    image(patImgs[5], windowWidth / 2 - 240, windowHeight / 2 - 120, 100, 100); // grapes
+    image(patImgs[6], windowWidth / 2 - 240, windowHeight / 2, 100, 100); // watermelon 
+    image(patImgs[7], windowWidth / 2 + 120, windowHeight / 2 - 120, 100, 100); // bomb just to make it even
   
 
 }
