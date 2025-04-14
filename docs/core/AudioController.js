3706b43f6f26cf5eef756a154bbaee267db85c89
Matilda Stokes <jl21579@bristol.ac.uk>
2025-04-14 19:36:22 +0100

    seperate music on/off and sound effects on/off

--- docs/core/AudioController.js
+++ docs/core/AudioController.js
@@ -1,24 +1,30 @@
 class AudioController {
     constructor () {
         this.sounds = {};
-        this.isPaused = true;
+        this.musicPaused = true;
+        this.effectsPaused = false;
     }
 
     preload () {
         this.sounds['slice'] = loadSound('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/sounds/soundSlicing.wav');
         this.sounds['bomb'] = loadSound('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/sounds/bombSound.wav');
+        this.sounds['button'] = loadSound('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/sounds/soundButtonClick.wav');
+        this.sounds['gameOver'] = loadSound('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/sounds/soundGameOver.wav');
+        this.sounds['lifeGained'] = loadSound('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/sounds/soundLifeGained.wav');
+        this.sounds['lifeLost'] = loadSound('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/sounds/soundLoseLife.wav');
+        this.sounds['recipe'] = loadSound('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/sounds/soundRecipeComplete.wav');
         this.sounds['theme'] = loadSound('https://raw.githubusercontent.com/UoB-COMSM0166/2025-group-20/main/docs/sounds/smoothieOperatorStart.wav');
     }
 
     play(name) {
-        if (!this.isPaused && this.sounds[name]) {
+        if (!this.effectsPaused && this.sounds[name] && name !== 'theme') {
             this.sounds[name].play();
         }
     }
 
-    toggleOnOff () {
-        this.isPaused = !this.isPaused;
-        if (!this.isPaused) {
+    toggleMusic () {
+        this.musicPaused = !this.musicPaused;
+        if (!this.musicPaused) {
             if (this.sounds['theme'] && !this.sounds['theme'].isPlaying()) {
               this.sounds['theme'].loop();
             }
@@ -29,10 +35,14 @@ class AudioController {
           }
     }
 
+    toggleEffects () {
+        this.effectsPaused = !this.effectsPaused;
+    }
+
     loop(name) {
         const sound = this.sounds[name];
         if (sound && !sound.isPlaying()) {
-          sound.setVolume(this.isPaused ? 0 : 1);
+          sound.setVolume(this.musicPaused ? 0 : 1);
           sound.loop();
         }
       }
