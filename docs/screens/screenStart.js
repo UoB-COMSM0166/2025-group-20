b8540e2666454ad16862a3e714528c064838d885
May Daoud <zy21368@bristol.ac.uk>
2025-04-21 12:19:24 +0100

    start screen cleanup

--- docs/screens/screenStart.js
+++ docs/screens/screenStart.js
@@ -1,29 +1,28 @@
-
 class ScreenStart {
   constructor() {
       this.fruitBorder = new FruitBorder();
       this.tutorialButton = new TextButton((windowWidth / 2) - 125, (windowHeight / 3 + 20), 'TUTORIAL', 250, 50, '25px', () => {
-          gameManager.switchState("tutorial-entry"); // Goes to tutorial screen screen
+         gameManager.switchState("tutorial-entry"); // Goes to tutorial screen screen
       });
       this.easyModeButton = new TextButton((windowWidth / 2) - 250, (windowHeight / 2), 'EASY MODE', 250, 50, '25px', () => {
-          difficulty = 'easy';
-          this.hardModeButton.getButton().style('background-color', '#FCF3CF');
-          this.easyModeButton.getButton().style('background-color', '#A89058');
+         gameManager.setDifficulty('easy');
+         this.hardModeButton.getButton().style('background-color', '#FCF3CF');
+         this.easyModeButton.getButton().style('background-color', '#A89058');
       });
       this.hardModeButton = new TextButton((windowWidth / 2) + 10, (windowHeight / 2), 'HARD MODE', 250, 50, '25px', () => {
-          difficulty = 'hard';
-          this.easyModeButton.getButton().style('background-color', '#FCF3CF');
-          this.hardModeButton.getButton().style('background-color', '#A89058');
+         gameManager.setDifficulty('hard');
+         this.easyModeButton.getButton().style('background-color', '#FCF3CF');
+         this.hardModeButton.getButton().style('background-color', '#A89058');
       });
       this.onePlayerButton = new TextButton((windowWidth / 2) - 250, (windowHeight / 2) + 70, 'ONE PLAYER', 250, 50, '25px', () => {
-          gameManager.twoPlayer = false;
-          this.twoPlayerButton.getButton().style('background-color', '#FCF3CF');
-          this.onePlayerButton.getButton().style('background-color', '#A89058');
+         gameManager.setCoop(false);
+         this.twoPlayerButton.getButton().style('background-color', '#FCF3CF');
+         this.onePlayerButton.getButton().style('background-color', '#A89058');
       });
       this.twoPlayerButton = new TextButton((windowWidth / 2) + 10, (windowHeight / 2) + 70,'TWO PLAYER', 250, 50, '25px', () => {
-          gameManager.twoPlayer = true;
-          this.onePlayerButton.getButton().style('background-color', '#FCF3CF');
-          this.twoPlayerButton.getButton().style('background-color', '#A89058');
+         gameManager.setCoop(true);
+         this.onePlayerButton.getButton().style('background-color', '#FCF3CF');
+         this.twoPlayerButton.getButton().style('background-color', '#A89058');
       });
       this.onePlayerButton.getButton().style('background-color', '#A89058');
       this.easyModeButton.getButton().style('background-color', '#A89058');
@@ -63,38 +62,36 @@ class ScreenStart {
   }
 
   drawTitle() {
-      textAlign(CENTER, CENTER);
-      textFont(gameFont);
-      fill('white');
-      stroke('black');
-      strokeWeight(6);
-      textSize(100);
-      text('Smoothie Operator', width / 2, height / 8);
-
-      textSize(30);
-      strokeWeight(4);
-      text('Choose your recipe for the game:', windowWidth / 2 , windowHeight / 2 - 35);
+   textAlign(CENTER, CENTER);
+   textFont(gameFont);
+   fill('white');
+   stroke('black');
+   strokeWeight(6);
+   textSize(100);
+   text('Smoothie Operator', width / 2, height / 8);
+   textSize(30);
+   strokeWeight(4);
+   text('Choose your recipe for the game:', windowWidth / 2 , windowHeight / 2 - 35);
   }
 
   drawWaveText() {
-      textAlign(CENTER, CENTER);
-      textFont(gameFont);
-      fill('white');
-      stroke('black');
-      strokeWeight(4);
-      textSize(40);
-      let textString = "Press ENTER to start game!";
-      let textLength = textString.length;
+   textAlign(CENTER, CENTER);
+   textFont(gameFont);
+   fill('white');
+   stroke('black');
+   strokeWeight(4);
+   textSize(40);
+   let textString = "Press ENTER to start game!";
+   let textLength = textString.length;
 
-      for (let i = 0; i < textLength; i++) {
-        let char = textString.charAt(i);
-        let yWave = sin(frameCount * yWaveSpeed + i * 0.5 + yWaveOffset) * yWaveSize;
-        let xWave = cos(frameCount * xWaveSpeed + i * 0.5 + xWaveOffset) * xWaveSize;
-        let xPos = (windowWidth / 2 + 30) + xWave + (i - textLength / 2) * 32;
-        let yPos = windowHeight / 8 + 445 + yWave;
-
-        text(char, xPos, yPos);
-      }
+   for (let i = 0; i < textLength; i++) {
+      let char = textString.charAt(i);
+      let yWave = sin(frameCount * yWaveSpeed + i * 0.5 + yWaveOffset) * yWaveSize;
+      let xWave = cos(frameCount * xWaveSpeed + i * 0.5 + xWaveOffset) * xWaveSize;
+      let xPos = (windowWidth / 2 + 30) + xWave + (i - textLength / 2) * 32;
+      let yPos = windowHeight / 8 + 445 + yWave;
+      text(char, xPos, yPos);
+   }
   }
 }
 
